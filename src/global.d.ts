d0cfe3427c03fed4dcffa8592ede8412251fdbeb
dodfrm <dodiprj0@gmail.com>
2026-05-30 21:14:11 +0700

    fix: update NavbarButton to use dynamic component rendering

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,14 +1,25 @@
+import React from "react";
+
 export { };
 
-declare module '*.glb';
-declare module '*.png';
+declare module "*.glb";
+declare module "*.png";
 
-declare module 'meshline' {
+declare module "meshline" {
   export const MeshLineGeometry: any;
   export const MeshLineMaterial: any;
 }
 
 declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      meshLineGeometry: any;
+      meshLineMaterial: any;
+    }
+  }
+}
+
+declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
       meshLineGeometry: any;
